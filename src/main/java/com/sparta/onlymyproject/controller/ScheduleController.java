57b1958c76da57109c1d272444581568390720c3
Imnotcoderdude <topsolo7@naver.com>
2024-05-28 11:55:15 +0900

    update : controller 의 구현해야할 api들 TODO 로 작성

--- src/main/java/com/sparta/onlymyproject/controller/ScheduleController.java
+++ src/main/java/com/sparta/onlymyproject/controller/ScheduleController.java
@@ -1,9 +1,43 @@
 package com.sparta.onlymyproject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sparta.onlymyproject.dtos.ScheduleRequestDto;
+import com.sparta.onlymyproject.dtos.ScheduleResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/schedule")
 public class ScheduleController {
+
+    @PostMapping
+    public ResponseEntity<ScheduleResponseDto> addSchedule(@RequestBody ScheduleRequestDto requestDto) {
+        // TODO : 일정을 입력받고 반환하는 api 작성하기
+        return null;
+    }
+
+    @GetMapping("/show")
+    public ResponseEntity<ScheduleResponseDto> getSchedule() {
+        // TODO : 선택한 일정을 조회하는 api 작성하기
+        return null;
+    }
+
+    @GetMapping("/show/all")
+    public ResponseEntity<ScheduleResponseDto> getAllSchedule() {
+        // TODO : 모든 일정 내역을 조회하는 api 작성하기
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ScheduleResponseDto> setSchedule(@RequestBody ScheduleRequestDto requestDto, @PathVariable int id) {
+        // TODO : 선택한 일정 수정하는 api 작성하기
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ScheduleResponseDto> deleteSchedule(@PathVariable int id) {
+        // TODO : 일정의 id를 사용하여 선택한 일정을 삭제하는 api 작성하기
+        return null;
+    }
 }
--- src/main/java/com/sparta/onlymyproject/entity/Schedule.java
+++ src/main/java/com/sparta/onlymyproject/entity/Schedule.java
@@ -1,9 +1,26 @@
 package com.sparta.onlymyproject.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 public class Schedule {
- }
+    // entity 사용을 위한 Id 설정.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String scheduleName; // 일정 제목
+    private String scheduleContent; // 일정 내용
+    private String scheduleUser; // 일정 작성자
+    private Long schedulePassword; // 일정 비밀번호
+    private LocalDateTime createdAt; // 일정 작성일자
+
+
+}
