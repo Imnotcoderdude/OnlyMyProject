d49dc843b6ba28e6adc2fc1fc3c9dd56a0764cf5
Imnotcoderdude <topsolo7@naver.com>
2024-05-28 11:05:20 +0900

    init commit

---
+++ src/main/java/com/sparta/onlymyproject/OnlyMyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.onlymyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyMyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyMyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/onlymyproject/OnlyMyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.onlymyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyMyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
