71e6a518a36ce7f1b32397db7fc2a861d7a0b437
Imnotcoderdude <topsolo7@naver.com>
2024-05-28 15:04:31 +0900

    fix : 입력된 데이터가 schedule 데이터베이스가 아닌 TodoProject DB에 저장되고 있는 것을 확인. application.properties 파일에서 DB 경로를 schedule DB로 변경하면서 수정완료.

--- src/main/java/com/sparta/onlymyproject/entity/Schedule.java
+++ src/main/java/com/sparta/onlymyproject/entity/Schedule.java
@@ -1,13 +1,11 @@
 package com.sparta.onlymyproject.entity;
 
 import com.sparta.onlymyproject.dtos.ScheduleRequestDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -19,13 +17,16 @@ public class Schedule {
     // entity 사용을 위한 Id 설정.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
 
     private String scheduleName; // 일정 제목
     private String scheduleContent; // 일정 내용
     private String scheduleUser; // 일정 작성자
     private String schedulePassword; // 일정 비밀번호
-    private LocalDateTime createdAt; // 일정 작성일자
+
+    @CreatedDate
+    private LocalDateTime createdAt; // 날짜
 
 
     // 클라이언트와 통신해서 받아온 데이터를 entity 에 넣어주기 위해서 생성자를 만들어서 사용한다.
@@ -35,7 +36,7 @@ public class Schedule {
         this.scheduleContent = requestDto.getScheduleContent();
         this.scheduleUser = requestDto.getScheduleUser();
         this.schedulePassword = requestDto.getSchedulePassword();
-
+        this.createdAt = LocalDateTime.now();
     }
 
     public Schedule(String scheduleName, String scheduleContent, String scheduleUser, String schedulePassword) {
