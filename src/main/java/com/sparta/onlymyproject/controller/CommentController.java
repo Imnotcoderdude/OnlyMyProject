3893f13611a5124353198b1e33399866b9ad8cd3
Imnotcoderdude <topsolo7@naver.com>
2024-05-31 12:38:42 +0900

    Update : 댓글 생성, 조회 기능에 ResponseEntity 메서드 추가로 상태코드 식별

--- src/main/java/com/sparta/onlymyproject/controller/CommentController.java
+++ src/main/java/com/sparta/onlymyproject/controller/CommentController.java
@@ -4,18 +4,22 @@ import com.sparta.onlymyproject.dtos.commentDto.CommentRequestDto;
 import com.sparta.onlymyproject.dtos.commentDto.CommentResponseDto;
 import com.sparta.onlymyproject.service.CommentService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+// 이 어노테이션을 제거한 이유는 생성자를 자동으로 생성되게 하지 않고 직접 만들어서 사용하는 방식이 좀더 컨트롤에 쉬울 것이라는 의견을 들었기 때문이다.
 //@AllArgsConstructor
 @RequestMapping("/api")
 public class CommentController {
 
     private final CommentService commentService;
 
+    // 생성자.
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
     }
@@ -23,14 +27,19 @@ public class CommentController {
 
     // 댓글을 생성하는 api. 댓글을 작성하고싶은 일정의 id를 입력해 그 일정에 댓글을 달 수 있게 해주는 작동방식.
     @PostMapping("/schedules/{scheduleId}/comments")
-    public CommentResponseDto addComment(@PathVariable Long scheduleId, @Valid @RequestBody CommentRequestDto commentRequestDto) {
-        return commentService.addComment(scheduleId, commentRequestDto);
+    // HTTP 상태코드 추가를 위해 ResponseEntity 클래스 사용.
+    public ResponseEntity<CommentResponseDto>  addComment(@PathVariable Long scheduleId, @Valid @RequestBody CommentRequestDto commentRequestDto) {
+        CommentResponseDto commentResponseDto = commentService.addComment(scheduleId, commentRequestDto);
+        // 반환에 CREATED 상태코드를 추가한다.
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentResponseDto);
     }
 
     // 요청된 일정에 대한 모든 댓글을 조회하는 api
     @GetMapping("/schedules/{scheduleId}/comments")
-    public List<CommentResponseDto> getAllComments(@PathVariable Long scheduleId) {
-        return commentService.getAllComment(scheduleId);
+    // ResponseEntity 추가.
+    public ResponseEntity<List<CommentResponseDto>> getAllComments(@PathVariable Long scheduleId) {
+       List<CommentResponseDto> commentResponseDto = commentService.getAllComment(scheduleId);
+        return ResponseEntity.ok(commentResponseDto);
     }
 
     // 댓글을 수정하는 api
